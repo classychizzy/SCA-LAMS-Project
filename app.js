29313b32adf6e0d2200fd7b6de2f5813aa9f6bff
Chizzy <chizobachukwuoma@gmail.com>
2020-06-16 14:59:33 +0100

    added database connection

--- app.js
+++ app.js
@@ -6,24 +6,31 @@ dotenv.config();
 
 const app = express();
 
+//app configuration
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
 // Mongodb connection string
-mongoose.connect(
-    process.env.DB_CONNECTION, {
+mongoose.connect( 
+    process.env.MONGO_URI
+, {
         useNewUrlParser: true,
         useUnifiedTopology: true
-    },
-    (err) => {
-        if (err) throw err;
-        else console.log('connected to db!')
+    }).then(()=>{
+        console.log("DB connected")
     });
-
+//error handling for the connection string
+const db = mongoose.connection;
+db.on('error',
+  console.error.bind(console, 'connection:error'));
+db.once('open', () => {
+  console.log(`DB connected on ${port}`)
+});
+  
 const port = process.env.PORT || 4000;
+// express server runs here
 app.listen(port, () => {
     console.log(`Server is up and running on port ${port}`);
 })
 
-module.exports = app;
